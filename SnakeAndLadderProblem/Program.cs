360278d5ebd415ae254d87fa69902f303b39cfb2
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-05 18:26:48 +0530

    Added to RepeatUntilWin

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -12,46 +12,58 @@ namespace SnakeAndLadderProblem
         {
             int playerPosition = 0, diceRoll, playerOption, listIndex;
             Random rand = new Random();
-            diceRoll = rand.Next(1, 7);
-            Console.WriteLine("Number appeared on dice is : " + diceRoll);
-
-            //Player Options generation "Array" "Logic 1"
             string[] choiceArray = { "noPlay", "ladder", "snake" };
-            listIndex = rand.Next(choiceArray.Length);
-            Console.WriteLine("Player Option is : " + choiceArray[listIndex]);
+            while (playerPosition < 100)
+            {
+                //Dice rolling logic
+                diceRoll = rand.Next(1, 7);
+                Console.WriteLine("Number appeared on dice is : " + diceRoll);
 
-            //Player Options generation "Random" "Logic 2"
-            /*playerOption = rand.Next(0, 3);
-            switch (playerOption)
-            {
-                case noPlay:
-                    Console.WriteLine("Player Option is : No Play");
-                    break;
-                case ladder:
-                    Console.WriteLine("Player Option is : Ladder");
-                    break ;
-                case snake:
-                    Console.WriteLine("Player Option is : Snake");
-                    break;
-            }*/
+                //Player Options generation "Array" "Logic 1"
+                listIndex = rand.Next(choiceArray.Length);
+                Console.WriteLine("Player Option is : " + choiceArray[listIndex]);
 
-            //Actions according to player option
-            if (choiceArray[listIndex] == "ladder")
-            {
-                playerPosition += diceRoll;
-            }
-            else if (choiceArray[listIndex] == "snake")
-            {
-                if (playerPosition < diceRoll)
+                //Player Options generation "Random" "Logic 2"
+                /*playerOption = rand.Next(0, 3);
+                switch (playerOption)
                 {
-                    playerPosition = 0;
-                }
-                else
+                    case noPlay:
+                        Console.WriteLine("Player Option is : No Play");
+                        break;
+                    case ladder:
+                        Console.WriteLine("Player Option is : Ladder");
+                        break ;
+                    case snake:
+                        Console.WriteLine("Player Option is : Snake");
+                        break;
+                }*/
+
+                //Actions according to player option
+                if (choiceArray[listIndex] == "ladder")
                 {
-                    playerPosition -= diceRoll;
+                    if ((playerPosition + diceRoll) > 100)
+                    {
+                        playerPosition = 100;
+                    }
+                    else
+                    {
+                        playerPosition += diceRoll;
+                    }
                 }
+                else if (choiceArray[listIndex] == "snake")
+                {
+                    if (playerPosition < diceRoll)
+                    {
+                        playerPosition = 0;
+                    }
+                    else
+                    {
+                        playerPosition -= diceRoll;
+                    }
+                }
+                Console.WriteLine("Player Position : " + playerPosition);
             }
-            Console.WriteLine("Player Position is : " + playerPosition);
+            Console.WriteLine("Last Player Position is : " + playerPosition);
         }
     }
 }
