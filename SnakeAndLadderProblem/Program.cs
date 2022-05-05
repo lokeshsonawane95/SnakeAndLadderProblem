567241b28ee62c9794e2e7e16356aa81110ed2d2
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-05 16:19:04 +0530

    Added to DiceRoll

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -6,8 +6,10 @@ namespace SnakeAndLadderProblem
     {
         public static void Main(string[] args)
         {
-            int playerPosition = 0;
-            Console.WriteLine(playerPosition);
+            int playerPosition = 0, diceRoll;
+            Random rand = new Random();
+            diceRoll = rand.Next(1, 7);
+            Console.WriteLine("Number appeared on dice is : " + diceRoll);
         }
     }
 }
