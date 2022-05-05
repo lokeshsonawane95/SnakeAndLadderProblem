0d5fe97b049e31f358665ac0b3a8a7e10dbc6134
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-05 16:12:55 +0530

    Added to PlayerPosition

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int playerPosition = 0;
+            Console.WriteLine(playerPosition);
+        }
+    }
+}
